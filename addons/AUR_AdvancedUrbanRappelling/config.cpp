2b96ba67231ea4fb595d9ac07ad560255a16e348
nicoman35 <christian_nicolau@yahoo.de>
2020-02-10 12:23:25 +0100

    Update config.cpp

--- addons/AUR_AdvancedUrbanRappelling/config.cpp
+++ addons/AUR_AdvancedUrbanRappelling/config.cpp
@@ -1,65 +1,49 @@
-class CfgPatches
-{
-	class AUR_AdvancedUrbanRappelling
-	{
+class CfgPatches {
+	class AUR_AdvancedUrbanRappelling {
 		author = "duda123";
 		name = "Advanced Urban Rappelling";
 		url = "https://github.com/sethduda/AdvancedUrbanRappelling";
 		units[] = {"AUR_AdvancedUrbanRappelling"};
 		requiredVersion = 1.0;
 		requiredAddons[] = {"A3_Modules_F"};
-		weapons[] = {"AUR_Rappel_Gear","AUR_Rappel_Rope"};
+		weapons[] = {
+			"AUR_Rappel_Gear",
+			"AUR_Rappel_Rope"
+		};
 	};
 };
-
-class CfgNetworkMessages
-{
-	
-	class AdvancedUrbanRappellingRemoteExecClient
-	{
+class CfgNetworkMessages {
+	class AdvancedUrbanRappellingRemoteExecClient {
 		module = "AdvancedUrbanRappelling";
 		parameters[] = {"ARRAY","STRING","OBJECT","BOOL"};
 	};
-	
-	class AdvancedUrbanRappellingRemoteExecServer
-	{
+	class AdvancedUrbanRappellingRemoteExecServer {
 		module = "AdvancedUrbanRappelling";
 		parameters[] = {"ARRAY","STRING","BOOL"};
 	};
-	
 };
-
-class CfgFunctions 
-{
-	class SA
-	{
-		class AdvancedUrbanRappelling
-		{
+class CfgFunctions {
+	class SA {
+		class AdvancedUrbanRappelling {
 			file = "\AUR_AdvancedUrbanRappelling\functions";
-			class advancedUrbanRappellingInit
-			{
+			class advancedUrbanRappellingInit {
 				postInit=1;
 			};
 		};
 	};
 };
-
-class CfgSounds
-{
-	class AUR_Rappel_Loop
-	{
+class CfgSounds {
+	class AUR_Rappel_Loop {
 		name = "AUR_Rappel_Loop";
 		sound[] = {"\AUR_AdvancedUrbanRappelling\sounds\AUR_Rappel_Loop.ogg", db+5, 1};
 		titles[] = {0,""};
 	};
-	class AUR_Rappel_Start
-	{
+	class AUR_Rappel_Start {
 		name = "AUR_Rappel_Start";
 		sound[] = {"\AUR_AdvancedUrbanRappelling\sounds\AUR_Rappel_Start.ogg", db+10, 1};
 		titles[] = {0,""};
 	};
-	class AUR_Rappel_End
-	{
+	class AUR_Rappel_End {
 		name = "AUR_Rappel_End";
 		sound[] = {"\AUR_AdvancedUrbanRappelling\sounds\AUR_Rappel_End.ogg", db+10, 1};
 		titles[] = {0,""};
@@ -73,6 +57,7 @@ class CfgWeapons {
 		displayName = "Rappelling Gear";
 		author = "vurtual";
 		model = "A3\Weapons_F\Items\ToolKit";
+		picture="\AUR_AdvancedUrbanRappelling\ui\m_harness_ca";
 		descriptionShort = "Essential rappelling gear, including harness, for urban rappelling.";
 		class ItemInfo: CBA_MiscItem_ItemInfo {
 			mass = 8;
@@ -80,10 +65,11 @@ class CfgWeapons {
 	};
 	class AUR_Rappel_Rope: CBA_MiscItem {
 		scope = 2;
-		displayName = "Rappelling Rope";
+		displayName = "Rappelling Rope 10 m";
 		author = "vurtual";
 		model = "\A3\Structures_F_Heli\Items\Tools\Rope_01_F.p3d";
-		descriptionShort = "10m rope - required to rappel down buildings.";
+		picture="\AUR_AdvancedUrbanRappelling\ui\m_rope_ca";
+		descriptionShort = "10m rope - required to rappel down buildings. Use multiple for higher altitudes.";
 		class ItemInfo: CBA_MiscItem_ItemInfo {
 			mass = 5;
 			};
