5be06eed584f025b92797f02003e2e3ffd81e8d4
nicoman35 <christian_nicolau@yahoo.de>
2020-02-19 08:48:27 +0100

    Update config.cpp

--- addons/AUR_AdvancedUrbanRappelling/config.cpp
+++ addons/AUR_AdvancedUrbanRappelling/config.cpp
@@ -1,12 +1,12 @@
 class CfgPatches {
 	class AUR_AdvancedUrbanRappelling {
-		author 			= "duda123";
-		name 			= "Advanced Urban Rappelling";
-		url 			= "https://github.com/sethduda/AdvancedUrbanRappelling";
-		units[] 		= {"AUR_AdvancedUrbanRappelling"};
+		author 				= "duda123";
+		name 				= "Advanced Urban Rappelling";
+		url 				= "https://github.com/sethduda/AdvancedUrbanRappelling";
+		units[] 			= {"AUR_AdvancedUrbanRappelling"};
 		requiredVersion 	= 1.0;
 		requiredAddons[] 	= {"A3_Modules_F"};
-		weapons[] 		= {
+		weapons[] 			= {
 			"AUR_Rappel_Gear",
 			"AUR_Rappel_Rope"
 		};
@@ -14,12 +14,12 @@ class CfgPatches {
 };
 class CfgNetworkMessages {
 	class AdvancedUrbanRappellingRemoteExecClient {
-		module 		= "AdvancedUrbanRappelling";
-		parameters[] 	= {"ARRAY","STRING","OBJECT","BOOL"};
+		module 			= "AdvancedUrbanRappelling";
+		parameters[] 	= {"ARRAY", "STRING", "OBJECT", "BOOL"};
 	};
 	class AdvancedUrbanRappellingRemoteExecServer {
-		module 		= "AdvancedUrbanRappelling";
-		parameters[] 	= {"ARRAY","STRING","BOOL"};
+		module 			= "AdvancedUrbanRappelling";
+		parameters[] 	= {"ARRAY", "STRING", "BOOL"};
 	};
 };
 class CfgFunctions {
@@ -53,23 +53,23 @@ class CfgWeapons {
 	class CBA_MiscItem;
 	class CBA_MiscItem_ItemInfo;
 	class AUR_Rappel_Gear: CBA_MiscItem {
-		scope 			= 2;
+		scope 				= 2;
 		displayName 		= $STR_AUR_RAPPELING_GEAR;
 		descriptionShort 	= $STR_AUR_RAPPELING_GEAR_DESCR;
-		author 			= "vurtual";
-		model 			= "A3\Weapons_F\Items\ToolKit";
-		picture 		= "\AUR_AdvancedUrbanRappelling\ui\m_harness_ca";
+		author 				= "vurtual";
+		model 				= "A3\Weapons_F\Items\ToolKit";
+		picture 			= "\AUR_AdvancedUrbanRappelling\ui\m_harness_ca";
 		class ItemInfo: CBA_MiscItem_ItemInfo {
 			mass = 8;
 		};
 	};
 	class AUR_Rappel_Rope: CBA_MiscItem {
-		scope 			= 2;
+		scope 				= 2;
 		displayName 		= $STR_AUR_RAPPELING_ROPE;
 		descriptionShort 	= $STR_AUR_RAPPELING_ROPE_DESCR;
-		author 			= "vurtual";
-		model			= "\A3\Structures_F_Heli\Items\Tools\Rope_01_F.p3d";
-		picture 		= "\AUR_AdvancedUrbanRappelling\ui\m_rope_ca";
+		author 				= "vurtual";
+		model				= "\A3\Structures_F_Heli\Items\Tools\Rope_01_F.p3d";
+		picture 			= "\AUR_AdvancedUrbanRappelling\ui\m_rope_ca";
 		class ItemInfo: CBA_MiscItem_ItemInfo {
 			mass = 5;
 		};
